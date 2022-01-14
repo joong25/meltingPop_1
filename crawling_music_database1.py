b8e513ce3b9cbcb9c3687c3b7ec95c0292cce252
Joonghyun <tbvjqlqlqlr@naver.com>
2022-01-14 17:43:11 +0900

    멜론 해외 차트 가수 및 가사 크롤링 후 데이터베이스에 삽입

---
+++ crawling_music_database1.py
@@ -0,0 +1,34 @@
+import pymysql
+from crawling_music_database2 import song_list, singer_list
+
+conn = pymysql.connect(host='localhost', user='root',
+                       password='1234', db='meltingpop_db_1', charset='utf8mb4')
+
+cursor = conn.cursor()
+
+try:
+
+    cursor.execute("set foreign_key_checks = 0")
+    cursor.execute("drop table if exists client cascade")
+    cursor.execute("drop table if exists song cascade")
+    cursor.execute("drop table if exists song_info cascade")
+    cursor.execute("set foreign_key_checks = 1")
+    cursor.execute("""create table client(
+                        CID varchar(20) not null,
+                        CPW varchar(20) not null,
+                        CEmail varchar(20) not null,
+                        primary key(CID))""")
+    cursor.execute("""create table song_info(
+                        song varchar(50),
+                        singer varchar(50), 
+                        lyric varchar(5000))""")
+    conn.commit()
+except:
+    conn.rollback()
+for i in song_list:
+    cursor.execute('insert into song_info(song) values("'+i+'")')
+for i in singer_list:
+    cursor.execute("insert into song_info(singer) values('"+i+"')")
+conn.commit()
+
+conn.close()
